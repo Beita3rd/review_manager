6b12926dd3a149c5cd1275e4a81c7e6348a99dd9
tabei <@gmail.com>
2021-03-17 14:21:32 +0900

    新規登録のエラーメッセージの表示

--- routes/users.js
+++ routes/users.js
@@ -6,43 +6,19 @@ const bcrypt = require('bcrypt');
 const ensurer = require('./authentication-ensurer');
 
 router.get('/signup', ensurer.ensureForLogin, (req, res) => {
-  res.render('signup');
+  res.render('signup', {isUsed: false});
 });
 
 router.post('/signup', ensurer.ensureForLogin,
-  //入力のチェック
-  (req, res, next) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    const errors = [];
-
-    if (name === '') {
-      errors.push('ユーザー名が空です');
-    }
-    if (email === '') {
-      errors.push('メールアドレスが空です');
-    }
-    if (password === '') {
-      errors.push('パスワードが空です');
-    }
-    
-    if (errors.length > 0) {
-      console.log(errors);
-      res.redirect('/signup');
-    } else {
-      next();
-    }
-  },
   //メールアドレスがすでに登録されているかのチェック
   (req, res, next) => {
     const email = req.body.email;
+    
     User.findOne({where: {email: email}}).then((user) => {
       if(user === null){
         next();
       } else{
-        console.log('This email address is used');
-        res.redirect('/users/signup');
+        res.render('signup', {isUsed: true});
       }
     });
   },
