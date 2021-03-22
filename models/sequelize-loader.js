5c5e1e4351928133e6669a19ea312ae5f469186b
tabei <@gmail.com>
2021-03-22 11:47:09 +0900

    a

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.HEROKU_POSTGRESQL_OLIVE_URL || 'postgres://postgres:postgres@localhost/review_manager'
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/review_manager'
  );
 
 module.exports = {
