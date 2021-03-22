d48a7e9ef3e009ee71a758d2a387588c61b49204
tabei <@gmail.com>
2021-03-22 12:53:17 +0900

    データベース

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,9 +1,15 @@
 'use strict';
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/review_manager'
- );
-
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/review_manager'
+//  );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+      ssl: true
+  }
+});
 module.exports = {
   database: sequelize,
   Sequelize: Sequelize
